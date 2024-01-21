efe2e99f52d9ff2b015c15536a1b0ae1697cfb1c
Olga <minaeva9@gmail.com>
2024-01-21 16:04:08 -0500

    view more event added in js

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const currentYear = new Date().getFullYear();
 console.log(currentYear);
 
 document.getElementById("year").textContent = currentYear;
+
+const viewMore = document.getElementById("view-more");
+
+viewMore.addEventListener("click", function () {
+  const elements = document.querySelectorAll(".card");
+  elements.forEach((element) => {
+    element.classList.remove("hidden");
+  });
+});
